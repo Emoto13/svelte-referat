4e295316d7ff964177e470f7237538313ad27568
Emilian Spasov <emilianspasov@Emilians-MacBook-Pro.local>
2023-04-12 20:14:34 +0300

    Add referat content

---
+++ javascript/toggleMenu.js
@@ -0,0 +1,22 @@
+function toggleMenu() {
+    let menu = document.getElementById("menu-content");
+    let currentDisplayStyle = menu.style.display;
+  
+    if (currentDisplayStyle === "block") {
+      // If menu is open, close it
+      menu.style.display = "none";
+    } else {
+      // If menu is closed, openIt
+      menu.style.display = "block";
+    }
+  }
+  
+  // close menu if user clicks outside of it
+  document.addEventListener("mouseup", function (e) {
+    let menu = document.getElementById("menu");
+    let menuContent = document.getElementById("menu-content");
+  
+    if (!menu.contains(e.target)) {
+      menuContent.style.display = "none";
+    }
+  });
